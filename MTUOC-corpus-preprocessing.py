f3c41a55d7afa7eeb31ca50c19b31514f46eed71
mtuoc <158745470+mtuoc@users.noreply.github.com>
2024-03-11 11:46:26 +0100

    Finding URLs improved.

--- MTUOC-corpus-preprocessing.py
+++ MTUOC-corpus-preprocessing.py
@@ -43,11 +43,19 @@ def file_len(fname):
 def findEMAILs(string): 
     email=re.findall('\S+@\S+', string)   
     return email
-    
+
+'''
 def findURLs(string): 
     regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
     url = re.findall(regex,string)       
     return [x[0] for x in url] 
+'''
+def findURLs(text):
+    # Regular expression for identifying URLs
+    url_pattern = re.compile(r'https?://\S+|www\.\S+')
+    # Find all matches using the regular expression
+    matches = re.findall(url_pattern, text)
+    return(matches)
     
 def replace_EMAILs(string,code="@EMAIL@"):
     EMAILs=findEMAILs(string)
